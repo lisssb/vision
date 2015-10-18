ee846114632e04bb0ec93fdee929cf1d644532ab
lisssb <liss.sb@gmail.com>
2015-10-18 20:29:54 +0200

    version where I change the range

--- test.py
+++ test.py
@@ -2,6 +2,27 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.cm as cm
 import numpy as np
-img = mpimg.imread('escilum.tif')
-plt.imshow(img, cmap=cm.Greys_r)
-plt.show()
+import Image
+import cv2
+
+
+#img = cv2.imread('escilum.tif',0)
+#print img
+#equ = cv2.equalizeHist(img)
+#print equ
+#res = np.hstack((img,equ)) #stacking images side-by-side
+#print res
+#cv2.imwrite('res.png',res)
+
+
+
+from PIL import Image
+
+myIm = Image.open("escilum.tif")
+
+myImResta = myIm.point(lambda x: x-100)
+print lambda x: x-100
+myImSuma = myIm.point(lambda x: x+100)
+
+myImResta.save("contraste-100.png")
+myImSuma.save("contraste+100.png")
--- tr_punt.py
+++ tr_punt.py
@@ -8,7 +8,10 @@ import cv2
 
 
 img = mpimg.imread('escilum.tif')
-p = plt.hist(img.flatten(), bins=300, normed=True)
+counts, bins, bars = plt.hist(img.flatten(), bins=300, normed=True)
+
+#plt.imshow(img, cmap=cm.Greys_r)
+
 
 
 #b = img[...,0]  # blue channel
@@ -18,10 +21,33 @@ p = plt.hist(img.flatten(), bins=300, normed=True)
 #plt.hist(g, bins=100, normed=True, histtype='step')
 #plt.hist(r, bins=100, normed=True, histtype='step')
 
+
+
+#print bins
+#plt.show()
+
+def tr_punt(hist, input_range=[], output_range=[]):
+    if len(input_range) != 2 or len(output_range) != 2:
+        print "The input_range and the output_range must be specified and its length must be 2"
+        return
+
+    result = []
+    for i in hist:
+        if i < input_range[0]:
+            result.append(0)
+        elif i > input_range[1]:
+            result.append(255)
+        else:
+            result.append( (i-input_range[0]) * output_range[1]/(input_range[1] - input_range[0]) )
+    return result
+
+r = tr_punt(bins, [51, 150], [0, 255])
+plt.clf()
 plt.title('Histogram of escilum')
 plt.grid(True)
+print r
+t = plt.hist(r,  bins=150, color='blue', normed=True)
+
 plt.show()
-
-
-
-#def tr_punt(hist, input_range, output_range):
+#plt.imshow(img, cmap=cm.Greys_r)
+#plt.show()
